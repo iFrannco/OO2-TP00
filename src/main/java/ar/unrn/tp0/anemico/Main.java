71d5c8772e664b3880308f4b3acb8511e19e48f9
iFrannco <fcabeza91@gmail.com>
2025-03-07 20:22:35 -0300

    quitando comentarios

--- src/main/java/ar/unrn/tp0/anemico/Main.java
+++ src/main/java/ar/unrn/tp0/anemico/Main.java
@@ -3,8 +3,6 @@ package ar.unrn.tp0.anemico;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         var fecha = new FechaNoAnemica();
